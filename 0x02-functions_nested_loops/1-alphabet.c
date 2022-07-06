8e2aa9f9695b8ccc053b000e5d821fd732e8bac9
ODINGAH001 <edwin.odinga123@gmail.com>
2022-07-06 10:39:27 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
-/*
- * File: 1-alphabet.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
-void print_alphabet(void)
+* print_alphabet -> prints the lowercase alphabets
+*
+*/
+
+void print_alphabet(void);
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
+    int j;
+    for (j = 'a'; j<= 'z'; j++)
+    {
+        _putchar(j);
+    }
+    _putchar(\'n');
 }
+
