42e78cb95c9c5e82337fc63a21b28c2bad6f6f88
ODINGAH001 <edwin.odinga123@gmail.com>
2022-08-02 16:56:50 -0700

    c++

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef SUM_H
 #define SUM_H
 
@@ -12,18 +11,3 @@
 #define SUM(x, y) ((x) + (y))
 
 #endif
-=======
-#ifndef SUM_H                                                                                                                                                                                   
-#define SUM_H                                                                                                                                                                                   
-                                                                                                                                                                                                
-/*                                                                                                                                                                                              
- * File: 4-sum.h                                                                                                                                                                                
- * Auth: Brennan D Baraban                                                                                                                                                                      
- * Desc: Header file that defines a function-like macro SUM(x, y)                                                                                                                               
- *       that computes the sum of the numbers x and y.                                                                                                                                          
- */                                                                                                                                                                                             
-                                                                                                                                                                                                
-#define SUM(x, y) ((x) + (y))                                                                                                                                                                   
-                                                                                                                                                                                                
-#endif 
->>>>>>> a3c5c0bb583bcfa5a22cc7ac11bc1ae32ca9d6dc
