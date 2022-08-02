537f69ada182b5281a81c0fb81cc4ffc87ee8d28
ODINGAH001 <edwin.odinga123@gmail.com>
2022-08-02 16:31:47 -0700

    c++

--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "1-pi.h"
 #include <stdio.h>
 
@@ -17,23 +16,3 @@ int main(void)
 	printf("%.3f\n", a);
 	return (0);
 }
-=======
-#include "1-pi.h"                                                                                                                                                                               
-#include <stdio.h>                                                                                                                                                                              
-                                                                                                                                                                                                
-/**                                                                                                                                                                                             
- * main - check the code for Holberton School students.                                                                                                                                         
- *                                                                                                                                                                                              
- * Return: Always 0.                                                                                                                                                                            
- */                                                                                                                                                                                             
-int main(void)                                                                                                                                                                                  
-{                                                                                                                                                                                               
-        float a;                                                                                                                                                                                
-        float r;                                                                                                                                                                                
-                                                                                                                                                                                                
-        r = 98;                                                                                                                                                                                 
-        a = PI * r * r;                                                                                                                                                                         
-        printf("%.3f\n", a);                                                                                                                                                                    
-        return (0);                                                                                                                                                                             
-}             
->>>>>>> a3c5c0bb583bcfa5a22cc7ac11bc1ae32ca9d6dc
