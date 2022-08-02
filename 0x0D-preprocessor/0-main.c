2bd3dfc4e9505bd8f334555504b14fe2fcf5e5ef
ODINGAH001 <edwin.odinga123@gmail.com>
2022-08-02 16:14:59 -0700

    c++

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#include "0-object_like_macro.h"
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int s;
-
-	s = 98 + SIZE;
-	printf("%d\n", s);
-	return (0);
-}
-=======
 #include "0-object_like_macro.h"                                                                                                                                                                
 #include <stdio.h>                                                                                                                                                                              
                                                                                                                                                                                                 
@@ -30,5 +12,5 @@ int main(void)
                                                                                                                                                                                                 
         s = 98 + SIZE;                                                                                                                                                                          
         printf("%d\n", s);                                                                                                                                                                      
-        return (0);   
->>>>>>> a3c5c0bb583bcfa5a22cc7ac11bc1ae32ca9d6dc
+        return (0);  
+}
