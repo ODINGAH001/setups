e852666da0832504782e30656cbe7f6901884e5d
EDWIN ODINGA <106967045+ODINGAH001@users.noreply.github.com>
2022-08-03 01:25:18 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -16,5 +16,4 @@ int main(void)
         printf("%s\n", __FILE__);                                                                                                                                                               
                                                                                                                                                                                                 
         return (0);                                                                                                                                                                             
-}                                                                                                                                                                                               
-~                    
+}                                                                                                                                                                                                                
