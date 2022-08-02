2bf94fb3e4b4327379c9c5f1938ed0e033d68926
ODINGAH001 <edwin.odinga123@gmail.com>
2022-08-02 16:23:35 -0700

    c++

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
@@ -12,18 +11,3 @@
 #define SIZE 1024
 
 #endif
-=======
-#ifndef OBJECT_LIKE_MACRO_H                                                                                                                                                                     
-#define OBJECT_LIKE_MACRO_H                                                                                                                                                                     
-                                                                                                                                                                                                
-/*                                                                                                                                                                                              
- * File: 0-object_like_macro.h                                                                                                                                                                  
- * Auth: Brennan D Baraban                                                                                                                                                                      
- * Desc: Header file that defines a macro named                                                                                                                                                 
- *       SIZE as an abbreviation for the token 1024.                                                                                                                                            
- */                                                                                                                                                                                             
-                                                                                                                                                                                                
-#define SIZE 1024                                                                                                                                                                               
-                                                                                                                                                                                                
-#endif 
->>>>>>> a3c5c0bb583bcfa5a22cc7ac11bc1ae32ca9d6dc
