0cf2e2c7dbb9a1495e2b77e06b64f8b3ac466bc5
ODINGAH001 <edwin.odinga123@gmail.com>
2022-08-02 16:41:54 -0700

    c++

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * File: 2-main.c
  * Auth: Brennan D Baraban
@@ -18,24 +17,3 @@ int main(void)
 
 	return (0);
 }
-=======
-/*                                                                                                                                                                                              
- * File: 2-main.c                                                                                                                                                                               
- * Auth: Brennan D Baraban                                                                                                                                                                      
- */                                                                                                                                                                                             
-                                                                                                                                                                                                
-#include <stdio.h>                                                                                                                                                                              
-                                                                                                                                                                                                
-/**                                                                                                                                                                                             
- * main - Prints the name of the file of the program                                                                                                                                            
- *        was compiled from, followed by a new line.                                                                                                                                            
- *                                                                                                                                                                                              
- * Return: Always 0.                                                                                                                                                                            
- */                                                                                                                                                                                             
-int main(void)                                                                                                                                                                                  
-{                                                                                                                                                                                               
-        printf("%s\n", __FILE__);                                                                                                                                                               
-                                                                                                                                                                                                
-        return (0);                                                                                                                                                                             
-}                                                                                                                                                                                                                
->>>>>>> a3c5c0bb583bcfa5a22cc7ac11bc1ae32ca9d6dc
